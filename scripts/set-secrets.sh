d1a7ce6e3c5d01d828130a08ebd64618e4cfc395
Georgi Kirilov <58916152+georgy-kirilov@users.noreply.github.com>
2024-06-21 20:10:13 +0300

    Update set-secrets.sh

--- scripts/set-secrets.sh
+++ scripts/set-secrets.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 read -p "Enter IP Address: " ip_address
-read -p "Enter SSH Password: " ssh_password
+read -sp "Enter SSH Password: " ssh_password
+echo
 read -p "Enter SSL Email: " ssl_email
 read -p "Enter Domain: " domain
 read -p "Enter App Name: " app_name
 
-gh secret set IP_ADDRESS -b"$ip_address"
-gh secret set SSH_PASSWORD -b"$ssh_password"
-gh secret set SSL_EMAIL -b"$ssl_email"
-gh secret set DOMAIN -b"$domain"
-gh secret set APP_NAME -b"$app_name"
+gh secret set IP_ADDRESS --body "$ip_address"
+gh secret set SSH_PASSWORD --body "$ssh_password"
+gh secret set SSL_EMAIL --body "$ssl_email"
+gh secret set DOMAIN --body "$domain"
+gh secret set APP_NAME --body "$app_name"
